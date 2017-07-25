17dd724f42677acad24c8442acd4acc923a732b1
yuralala8 <yc1435@nyu.edu>
2017-07-25 17:58:30 -0400

    crud with validations

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,53 @@
+class SongsController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def index
+    @songs = Song.all
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.create(song_params)
+    if @song.valid?
+      @song.save
+    redirect_to song_path(@song)
+    else
+    render :new
+    end
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(song_params)
+    if @song.valid?
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @song = Song.find(params[:id]).destroy
+    redirect_to songs_path
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+  end
+
+end
